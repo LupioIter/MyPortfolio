374bf737a8940df35566a110e86c95103266d115
Lupio_Оля <skyuii389@gmail.com>
2022-09-19 22:16:24 +0300

    Initial commit

---
+++ src/skills/Skills.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import s from './skills.module.css';
+import sContainer from '../common/styles/Container.module.css'
+import Skill from "./skill/Skill";
+
+function Skills() {
+    return (
+        <div className={s.skillsBlock}>
+            <div className={'${sContainer.container} ${s.skillsContainer}'}>
+                <h2>Skills</h2>
+                <div className={s.skills}>
+                    <Skill title={"Js"} description={"Lorem ipsum"}/>
+                    <Skill title={"CSS"} description={"Lorem ipsum"}/>
+                    <Skill title={"React"} description={"Lorem ipsum"}/>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Skills;
